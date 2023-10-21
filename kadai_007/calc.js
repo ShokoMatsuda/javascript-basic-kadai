5a7088942cf00ff0bd1d14ad81fa596e7e605709
ShokoMatsuda <142991303+ShokoMatsuda@users.noreply.github.com>
2023-10-21 18:54:06 +0900

    修正

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -4,9 +4,14 @@ let num = Math.floor(Math.random() * 16);
 // 変数numの値を出力する（確認用）
 console.log(num);
 
-if (num > 0) {
+// 変数(num)が3と5の倍数の場合「3と5の倍数です」という文字列を出力する
+if (num > 0){
+    if ((num % 5 === 0) && (num % 3 === 0)) {
+        console.log('3と5の倍数です');
+    }
+
     // 変数(num)が3の倍数の場合「3の倍数です」という文字列を出力する
-    if (num % 3 === 0) {
+    else if (num % 3 === 0) {
         console.log('3の倍数です');
     } 
 
@@ -14,10 +19,6 @@ if (num > 0) {
     else if (num % 5 === 0) {
         console.log('5の倍数です');
     }
-
-    else if ((num % 5 === 0) && (num % 3 === 0)) {
-        console.log('3と5の倍数です');
-    } 
 
     else {
         console.log(num); 
@@ -27,26 +28,3 @@ if (num > 0) {
 else {
     console.log(num);
 }
-
-// 変数(num)が3の倍数の場合「3の倍数です」という文字列を出力する
-// if ((num > 0) && (num % 3 === 0)) {
-//     console.log('3の倍数です');
-//      // 変数(num)が3と5の倍数の場合「3と5の倍数です」という文字列を出力する
-//      if (num % 5 === 0) {
-//         console.log('3と5の倍数です');
-//      }
-//  }
-// // 変数(num)が5の倍数の場合「5の倍数です」という文字列を出力する
-// else if ((num > 0) && (num % 5 === 0)) {
-//      console.log('5の倍数です');
-//  }
-
-//  // 変数(num)が3と5の倍数の場合「3と5の倍数です」という文字列を出力する
-//   else if ((num % 3 === 0) && (num % 5 === 0)) {
-//       console.log('3と5の倍数です');
-//   }
-
-// // それ以外の場合は変数(num)を出力する
-//  else {
-//      console.log(num);
-//  }
